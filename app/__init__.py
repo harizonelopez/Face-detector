66fef900b081335956565357bbcc86e1dc35cde3
Harizone lopez <harizonelopez23@gmail.com>
2025-06-06 16:04:32 +0300

    Refactor Flask app initialization and update home-style.css background color

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,8 @@
 from flask import Flask
+import os
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__,  static_folder=os.path.join(os.path.dirname(__file__), 'static'))
     app.config['SECRET_KEY'] = 'J@rvis_007'
     app.config['UPLOAD_FOLDER'] = 'static/face_data'
 
@@ -9,4 +10,3 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
 
     return app
-
